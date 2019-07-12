b02deeb2f8e0fd58eb1be963bc52a1173dcc7ecf
Ben Lardie <benlardie@gmail.com>
2019-07-12 00:16:45 -0400

    schema is all set up and so is pagination

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,65 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
+async function signup(parent, args, context, info) {
+    const password = await bcrypt.hash(args.password, 10)
+    const user = await context.prisma.createUser({...args, password })
+    const token = jwt.sign({userId: user.id }, APP_SECRET)
+
+    return {
+        token,
+        user,
+    }
+}
+
+async function login(parent, args, context, info) {
+    const user = await context.prisma.user( {email: args.email} )
+    if (!user) {
+        throw new Error('No such user found')
+    }
+
+    const vaild = await bcrypt.compare(args.password, user.password)
+    if (!vaild) {
+        throw new Error('Invalid password')
+    }
+
+    const token = jwt.sign({userId: user.id}, APP_SECRET)
+
+    return {
+        token,
+        user,
+    }
+}
+
+function post(parent, args, context, info) {
+    const userId = getUserId(context)
+    return context.prisma.createLink({
+        url: args.url,
+        description: args.description,
+        postedBy: { connect: { id: userId } },
+    })
+}
+
+async function vote(parent, args, context, info) {
+    const userId = getUserId(context)
+    const linkExist = await context.prisma.$exist.vote({
+        user: { id: userId },
+        link: { id: args.linkId },
+    })
+    if (linkExist) {
+        throw new Error(`Already voted for link: ${args.linkId}`)
+    }
+    return context.prisma.createVote({
+        user: { connect: { id: userId }},
+        link: { connect: { id: args.linkId }}
+    })
+}
+
+module.exports = {
+    signup,
+    login,
+    post,
+    vote,
+}
+
